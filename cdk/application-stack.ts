0d40e17a601ea9cb9812a8983d16a0d0289e9d2f
James Earlywine <james@esilogix.com>
2023-09-04 00:09:27 -0400

    ..

--- cdk/application-stack.ts
+++ cdk/application-stack.ts
@@ -3,11 +3,12 @@ import {IRole} from "aws-cdk-lib/aws-iam";
 
 export class ApplicationStack {
     stack: cdk.Stack;
-    mergeDocumentAndDataLambda: cdk.aws_lambda.Function;
     lambdaExecutionRole: cdk.aws_iam.Role;
+    mergeDocumentAndDataLambda: cdk.aws_lambda.Function;
+
     constructor(app, id: string) {
         this.stack = new cdk.Stack(app, id);
-        
+
         this.lambdaExecutionRole = new cdk.aws_iam.Role(this.stack, "LambdaExecutionRole", {
             assumedBy: new cdk.aws_iam.ServicePrincipal('lambda.amazonaws.com'),
             description: 'Lambda Execution Role',
