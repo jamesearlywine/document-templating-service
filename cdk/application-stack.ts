f1d992078fd9e682a96ab5ae353112831e355a70
James Earlywine <james@esilogix.com>
2023-09-03 23:57:35 -0400

    ..

--- cdk/application-stack.ts
+++ cdk/application-stack.ts
@@ -3,12 +3,22 @@ import * as cdk from "aws-cdk-lib";
 export class ApplicationStack {
     stack: cdk.Stack;
     mergeDocumentAndDataLambda: cdk.aws_lambda.Function;
+    lambdaExecutionRole: cdk.aws_iam.Role;
     constructor(app, id: string) {
         this.stack = new cdk.Stack(app, id);
+        this.lambdaExecutionRole = new cdk.aws_iam.Role(this.stack, "LambdaExecutionRole", {
+            assumedBy: new cdk.aws_iam.ServicePrincipal('lambda.amazonaws.com'),
+            description: 'Lambda Execution Role',
+            managedPolicies: [
+                cdk.aws_iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"),
+            ]
+        });
+
         this.mergeDocumentAndDataLambda = new cdk.aws_lambda.Function(this.stack, "mergeDocumentAndData", {
             runtime: cdk.aws_lambda.Runtime.NODEJS_18_X,
             handler: "mergeDocumentAndData.handler",
             code: cdk.aws_lambda.Code.fromAsset(`${__dirname}/../build/handlers/mergeDocumentAndData`),
+            role: this.lambdaExecutionRole,
         });
     }
 }
