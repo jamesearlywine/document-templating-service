69f4e1211a6ea7325bbc1738bfaded4eea628f28
James Earlywine <james@esilogix.com>
2023-09-11 00:13:02 -0400

    ..

--- testing/misc/gotenberg-test.sh
+++ testing/misc/gotenberg-test.sh
@@ -2,6 +2,6 @@
 
 curl \
 --request POST 'http://localhost:3000/forms/libreoffice/convert' \
---form 'files=@"/Users/jearlywine/dev/processproof/document-templating-service/testing/test-generated-files/output--2364ca70-893f-4b8f-869b-ce01122deb62.docx"' \
+--form 'files=@"/Users/jearlywine/dev/processproof/document-templating-service/testing/test-generated-files/output--b0cfb9f7-be9d-446f-84d7-f02ae8ff79b7.docx"' \
 --form 'nativePdfFormat="PDF/A-1a"' \
--o output--2364ca70-893f-4b8f-869b-ce01122deb62.pdf
+-o output--b0cfb9f7-be9d-446f-84d7-f02ae8ff79b7.pdf
