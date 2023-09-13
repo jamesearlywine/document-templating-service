c16f391154952f5b8616aa0d4e9e9dc339172111
James Earlywine <james@esilogix.com>
2023-09-13 17:42:40 -0400

    ..

---
+++ src/config/index.ts
@@ -0,0 +1,2 @@
+import DocumentConversionServiceConfig from "src/services/document-conversion-service/document-conversion-service.config";
+import { ServiceConfig } from "src/config/lib/service-config";
