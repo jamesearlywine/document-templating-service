73e6b4d9fcfddb12ff9b6996302b8ecb80ec633f
James Earlywine <james@esilogix.com>
2023-09-13 23:49:13 -0400

    ..

--- testing/integration/docxtemplater-gotenberg.test.ts
+++ testing/integration/docxtemplater-gotenberg.test.ts
@@ -1,9 +1,10 @@
 import path from "path";
-import { validJobAffidavitTemplateData } from "../../src/data/template-data/job-affidavit/job-affidavit-template-data.fixtures";
+import fs from "fs";
+import waitForExpect from "wait-for-expect";
 import { v4 as uuid } from "uuid";
-import * as DocxTemplater from "../../src/services/document-templating-service/docxtemplater";
-import * as DocumentConversionService from "../../src/services/document-conversion-service";
-//@todo figure out absolute-path module resolution when running jest in a subfolder
+import { validJobAffidavitTemplateData } from "src/data/template-data/job-affidavit/job-affidavit-template-data.fixtures";
+import * as DocxTemplater from "src/services/document-templating-service/docxtemplater";
+import * as DocumentConversionService from "src/services/document-conversion-service";
 
 const TEMPLATE_FILE_PATH = path.resolve(
   `${__dirname}/../test-templates/AFFIDAVIT-OF-NON-SERVICE--DOCXTEMPLATER.docx`,
@@ -19,7 +20,7 @@ const OUTPUT_PDF_FILE_PATH = path.resolve(
 
 describe("local end-to-end - docxtemplater->gotenberg", () => {
   describe("docxtemplater->gotenberg", () => {
-    it("should generate a pdf from a docx template and data", async () => {
+    it("should generate a .docx and .pdf output from template .docx and data", async () => {
       DocxTemplater.generateTemplatedContent({
         templateFilepath: TEMPLATE_FILE_PATH,
         data: TEST_AFFIDAVIT_TEMPLATE_DATA,
@@ -31,7 +32,18 @@ describe("local end-to-end - docxtemplater->gotenberg", () => {
         outputLocation: OUTPUT_PDF_FILE_PATH,
       });
 
-      expect(true).toBe(true);
+      waitForExpect(() => {
+        expect(fs.existsSync(OUTPUT_DOCX_FILE_PATH)).toBe(true);
+
+        // Delete the output files (comment-out to view the outputs testing/test-generated-files/*)
+        fs.unlinkSync(OUTPUT_DOCX_FILE_PATH);
+      }, 5000);
+      waitForExpect(() => {
+        expect(fs.existsSync(OUTPUT_PDF_FILE_PATH)).toBe(true);
+
+        // Delete the output files (comment-out to view the outputs testing/test-generated-files/*)
+        fs.unlinkSync(OUTPUT_PDF_FILE_PATH);
+      }, 5000);
     });
   });
 });
