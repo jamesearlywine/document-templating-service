be8216634036a4f5712da70db397e0eda777f1ea
James Earlywine <james@esilogix.com>
2023-09-14 23:12:00 -0400

    ..

--- src/services/document-conversion-service/document-conversion-service.config.test.ts
+++ src/services/document-conversion-service/document-conversion-service.config.test.ts
@@ -1,4 +1,6 @@
-import DocumentConversionServiceConfig from "src/services/document-conversion-service/document-conversion-service.config";
+import DocumentConversionServiceConfig, {
+  ConfigKeys,
+} from "src/services/document-conversion-service/document-conversion-service.config";
 
 describe("DocumentConversionServiceConfig", () => {
   describe("set()", () => {
@@ -18,6 +20,39 @@ describe("DocumentConversionServiceConfig", () => {
       DocumentConversionServiceConfig.set({
         GOTENBERG_BASE_URL: MOCK_CONFIG_VALUE,
       });
+
+      expect(DocumentConversionServiceConfig.GOTENBERG_BASE_URL).toBe(
+        MOCK_CONFIG_VALUE,
+      );
+    });
+  });
+
+  describe("reset()", () => {
+    it("should reset the config values to their defaults", () => {
+      const MOCK_CONFIG_VALUE = "http://some-other-url.com";
+
+      DocumentConversionServiceConfig.set({
+        GOTENBERG_BASE_URL: MOCK_CONFIG_VALUE,
+      });
+
+      expect(DocumentConversionServiceConfig.GOTENBERG_BASE_URL).toBe(
+        MOCK_CONFIG_VALUE,
+      );
+
+      DocumentConversionServiceConfig.reset();
+
+      expect(DocumentConversionServiceConfig.GOTENBERG_BASE_URL).toBe(
+        DocumentConversionServiceConfig.DEFAULT_VALUES.GOTENBERG_BASE_URL,
+      );
+    });
+  });
+
+  describe("initialize()", () => {
+    it("should initialize the config to values set in process.env", async () => {
+      const MOCK_CONFIG_VALUE = "http://some-gotenberg-url.com";
+      process.env[ConfigKeys.GOTENBERG_BASE_URL] = MOCK_CONFIG_VALUE;
+
+      await DocumentConversionServiceConfig.initialize();
 
       expect(DocumentConversionServiceConfig.GOTENBERG_BASE_URL).toBe(
         MOCK_CONFIG_VALUE,
