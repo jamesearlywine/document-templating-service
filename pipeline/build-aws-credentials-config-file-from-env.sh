2651f5c308bafc53990db064467f5dc29e310ce6
James Earlywine <james@esilogix.com>
2023-09-17 13:43:17 -0400

    ..

--- pipeline/build-aws-credentials-config-file-from-env.sh
+++ pipeline/build-aws-credentials-config-file-from-env.sh
@@ -5,3 +5,9 @@ echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> ~/.aws/config
 echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> ~/.aws/config
 echo "region = us-east-2" >> ~/.aws/config
 echo "output = json" >> ~/.aws/config
+
+echo "[default]" > ~/.aws/credentials
+echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> ~/.aws/credentials
+echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> ~/.aws/credentials
+echo "region = us-east-2" >> ~/.aws/credentials
+echo "output = json" >> ~/.aws/credentials
