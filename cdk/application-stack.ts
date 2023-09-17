e27dc03ca734a19af46e72bb35e3777e5ed94b53
James Earlywine <james@esilogix.com>
2023-09-17 15:58:37 -0400

    ..

--- cdk/application-stack.ts
+++ cdk/application-stack.ts
@@ -1,13 +1,22 @@
 import * as cdk from "aws-cdk-lib";
 import { IRole } from "aws-cdk-lib/aws-iam";
+import { CfnParameter } from "aws-cdk-lib";
 
 export class ApplicationStack {
   stack: cdk.Stack;
+  AWS_ENV_Parameter: CfnParameter;
   lambdaExecutionRole: cdk.aws_iam.Role;
   mergeDocumentAndDataLambda: cdk.aws_lambda.Function;
 
   constructor(app, id: string) {
     this.stack = new cdk.Stack(app, id);
+
+    this.AWS_ENV_Parameter = new CfnParameter(this.stack, "AWS_ENV", {
+      type: "String",
+      description: "The AWS environment deployed to",
+      default: "DEV",
+      allowedValues: ["DEV", "TEST", "STAGING", "PROD"],
+    });
 
     this.lambdaExecutionRole = new cdk.aws_iam.Role(
       this.stack,
@@ -32,6 +41,15 @@ export class ApplicationStack {
         code: cdk.aws_lambda.Code.fromAsset(
           "build/handlers/mergeDocumentAndData",
         ),
+        environment: {
+          AWS_ENV: this.AWS_ENV_Parameter.valueAsString,
+          GOTENBERG_URL: cdk.Fn.sub(
+            "{{resolve:ssm:/${AWS_ENV}/document-templating-service/gotenberg-base-url:1}}",
+            {
+              AWS_ENV: this.AWS_ENV_Parameter.valueAsString,
+            },
+          ),
+        },
         role: this.lambdaExecutionRole as IRole,
       },
     );
