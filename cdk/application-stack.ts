b811d9e1089698e09c5c9c944f723296b4a9af1f
James Earlywine <james@esilogix.com>
2023-09-18 02:29:34 -0400

    ..

--- cdk/application-stack.ts
+++ cdk/application-stack.ts
@@ -71,7 +71,9 @@ export class ApplicationStack {
           cdk.aws_ec2.InstanceClass.C6GD,
           cdk.aws_ec2.InstanceSize.MEDIUM,
         ),
-        machineImage: cdk.aws_ec2.MachineImage.latestAmazonLinux2023(),
+        machineImage: cdk.aws_ec2.MachineImage.latestAmazonLinux2023({
+          cpuType: cdk.aws_ec2.AmazonLinuxCpuType.ARM_64,
+        }),
         vpc: this.vpc,
         securityGroup: this.gotenbergServiceSecurityGroup,
       } as InstanceProps,
