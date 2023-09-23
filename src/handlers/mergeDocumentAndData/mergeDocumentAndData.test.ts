c2f6911cd01fca8ce4282ce9ed88fde13d72727c
James Earlywine <james@esilogix.com>
2023-09-23 01:09:43 -0400

    ..

--- src/handlers/mergeDocumentAndData/mergeDocumentAndData.test.ts
+++ src/handlers/mergeDocumentAndData/mergeDocumentAndData.test.ts
@@ -1,17 +1,28 @@
 import { handler } from "./mergeDocumentAndData";
+import axios from "axios";
+jest.mock("axios", () => ({
+  ...jest.requireActual("axios"),
+  get: jest.fn(),
+}));
+
+const mockAxios = jest.mocked(axios);
 
 describe("mergeDocumentAndData", () => {
   describe("handler", () => {
-    it("should return event in response)", async () => {
+    it("should return a response)", async () => {
       const event = { mock: "event" };
 
       const response = await handler(event);
 
-      expect(response).toStrictEqual(
-        expect.objectContaining({
-          event,
-        }),
-      );
+      expect(response).toBeTruthy();
+    });
+
+    it("should call axios.get()", async () => {
+      const event = { mock: "event" };
+
+      const response = await handler(event);
+
+      expect(axios.get).toHaveBeenCalled();
     });
   });
 });
