4d8fa79f52814b3f702c82dcbbf77eda16546979
James Earlywine <james@esilogix.com>
2023-10-23 00:56:28 -0400

    ..

---
+++ testing/e2e/generate-documents.integration-test.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const endpoint =
+  " https://d2ecpkrcmf.execute-api.us-east-2.amazonaws.com/generateDocument";
+const requestBody = {
+  actorName: "Aethen",
+  color: "brown",
+  liquid: "mop-waters",
+};
+const templateId = "d192e487-d55f-447b-85d9-d2aad3e4fd5e";
+
+describe("generateDocuments endpoint", () => {
+  it("should generate documents", async () => {
+    const numberOfTests = 20;
+    let results;
+    try {
+      results = await Promise.all(
+        Array(numberOfTests)
+          .fill(0)
+          .map(() => axios.post([endpoint, templateId].join("/"), requestBody)),
+      );
+    } catch (error) {
+      console.log(error);
+    }
+
+    expect(results.length).toBe(numberOfTests);
+    expect(
+      results.map((result) => result.status).every((status) => status === 200),
+    ).toBe(true);
+  });
+});
---
+++ testing/e2e/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  rootDir: "../..",
+  testMatch: ["<rootDir>/testing/e2e/**/*.integration-test.ts"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  clearMocks: true,
+  collectCoverage: true,
+  coverageReporters: ["lcov", "text-summary"],
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  testTimeout: 180000,
+};
+
+export default config;
